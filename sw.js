bfde34d03f71f6f905cf3892b68408a792a4b885
Marvic Ranger <marvicranger01@gmail.com>
2026-05-31 18:04:11 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+// Service Worker for Connect Point (PWA)
+const CACHE_NAME = 'cp-cache-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/src/styles.css',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((networkResponse) => {
+        if (networkResponse.ok) {
+          const clone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        }
+        return networkResponse;
+      });
+    })
+  );
+});
