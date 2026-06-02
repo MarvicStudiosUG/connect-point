fc4427603954ff685dc8934344b1a169fe92d4fc
Marvic Ranger <marvicranger01@gmail.com>
2026-06-02 19:54:18 +0300

    Update UserContext.js

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,15 +1,8 @@
 import React, { createContext, useContext } from 'react';
-
 const UserContext = createContext(null);
-
 export function UserProvider({ user, children }) {
-  return React.createElement(
-    UserContext.Provider,
-    { value: user },
-    children
-  );
+  return React.createElement(UserContext.Provider, { value: user }, children);
 }
-
 export function useUser() {
   return useContext(UserContext);
 }
